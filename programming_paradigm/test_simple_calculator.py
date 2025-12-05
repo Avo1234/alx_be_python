733ba77f412debf6c77204a8c8a90c1bf4ab8f21
Herbert Asante <herbertasante66@gmail.com>
2025-12-05 23:53:01 +0000

    Unit Tests for a Simple Calculator Class

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,35 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(5, 10), -5)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+
+    def multiply(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(5, 0), 0)
+
+    def divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(10, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
